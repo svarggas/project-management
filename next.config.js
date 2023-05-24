a7aecc0a4eb18eb1c21fbde440a452066ffa1db3
Sebastian Vargas R <s.varggas@gmail.com>
2023-05-23 23:43:45 -0600

    Setting up deploy process to ignore things done from the workflow

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    /**
+     * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+     * out.
+     *
+     * @see https://github.com/vercel/next.js/issues/41980
+     */
+    i18n: {
+      locales: ["en"],
+      defaultLocale: "en",
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    swcMinify: true,
+};
 
 module.exports = nextConfig
