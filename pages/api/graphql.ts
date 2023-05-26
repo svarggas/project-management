978fe228337e2c0de348ce476583d5831873180b
Sebastian Vargas R <s.varggas@gmail.com>
2023-05-25 23:55:52 -0600

    Server handling/start with custom context

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,29 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server';
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { PrismaClient } from '@prisma/client';
 
 import typeDefs from '~/graphql/graph';
+import resolvers from '~/graphql/resolvers';
+import { CustomContext } from '~/graphql/index.d';
+import initContext from '~/graphql/context';
 
-const prisma = new PrismaClient();
-
-const resolvers = {
-  Query: {
-    hello: () => 'world',
-    getOrganization: async (_parent: any, args: any, _ctx: any) => {
-      const { id } = args;
-      const data = await prisma.organization.findUnique({
-        where: { id }
-      });
-      return data;
-    },
-  }
-};
-
-const server = new ApolloServer({
+const server = new ApolloServer<CustomContext>({
   typeDefs,
   resolvers,
 });
 
-export default startServerAndCreateNextHandler(server, {
-  context: async (req, res) => ({ req, res }),
+export default startServerAndCreateNextHandler<NextRequest, CustomContext>(server, {
+  context: initContext(),
 });
